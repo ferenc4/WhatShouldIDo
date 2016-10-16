eeb612b794ef6d51db8f22db2ec284790dacec13
Ferenc <ferenc4.fazekas@gmail.com>
2016-10-16 19:51:01 +1100

    added pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var homepageData = {
+    options: [
+        "Learn Something New",
+        "Watch Something Interesting",
+        "Plan An Event",
+        "Go Places",
+        "Do Sports",
+        "Check Out Social media",
+        "Take Care Of Yourself"
+    ],
+    type: 'choice',
+    name: 'YourName'
+};
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {name: 'YourName'});
+    var options = ["Learn Things", "Watch Stuff", "Plan An Event", "Going Places", "Do Sports", "Check Out Social media"];
+    res.render('index', homepageData);
 });
 
 module.exports = router;
